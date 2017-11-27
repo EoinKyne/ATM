8c829d2ed926a11ef787470e2055624f709a65d4
EoinKyne <eoin_kyne@yahoo.com>
2017-11-27 21:05:00 +0000

    Commit-feature/Start-ATM

--- src/ie/atm/ATM.java
+++ src/ie/atm/ATM.java
@@ -2,44 +2,63 @@ package ie.atm;
 
 public class ATM {
 
-	private boolean userAuthenticated;
-	private int	currentAccountNumber;
-	private Screen screen;
-	private Keypad keypad;
-	private CashDispenser cashDispenser;
-	private DepositSlot depositSlot;
-	private BankDatabase bankDatabase;
+	private boolean userAuthenticated; //whether user is authenticated
+	private int	currentAccountNumber; //current user's account number
+	private Screen screen;//ATM screen
+	private Keypad keypad; //ATM keypad
+	private CashDispenser cashDispenser; // ATM cash dispenser
+	private DepositSlot depositSlot; // ATM deposit slot
+	private BankDatabase bankDatabase; // account information db
 	
+	// Main menu -  constants corresponding to menu options
 	private static final int BALANCE_INQUIRY = 1;
 	private static final int WITHDRAWAL = 2;
 	private static final int DEPOSIT = 3;
 	private static final int EXIT = 4;
 	
-	
+	// Constuctor to initialise instance vbls
 	public ATM() {
 		
-		userAuthenticated = false;
-		currentAccountNumber = 0;
-		screen = new Screen();
-		keypad = new Keypad();
-		cashDispenser = new CashDispenser();
-		depositSlot = new DepositSlot();
-		bankDatabase = new BankDatabase();
+		userAuthenticated = false; //user is not authenticated to start
+		currentAccountNumber = 0; // No current account number to start
+		screen = new Screen(); // create screen
+		keypad = new Keypad(); // create keypad
+		cashDispenser = new CashDispenser(); //create cash dispenser
+		depositSlot = new DepositSlot(); // create deposit slot
+		bankDatabase = new BankDatabase(); // create account info db
 	}
 	
-	
+	// start ATM
+	public void run(){
+		// welcome and authenticate user and perform transaction
+		while (true){
+			// loop while user is not yet authenticated
+			while ( !userAuthenticated ){
+				screen.displayMessageLine("\nWelcome");
+				authenticateUser(); // authenticate user
+			}
+			performTransactions(); //user is now authenticated
+			userAuthenticated = false; // reset before next ATM session
+			currentAccountNumber = 0; // reset before next ATM session
+			screen.displayMessageLine("\nThank You. Goodbye");
+		}
+	}
+	// attempt to authenticate user
 	private void authenticateUser(){
 		
 	}
 	
+	// display main menu and perform transactions
 	private void performTransactions(){
 		
 	}
 	
+	// display main menu and return input selection
 	private int displayMainMenu(){
 		return 0;
 	}
 	
+	//return object of specified transaction subclass
 	private Transaction createTransaction(int type){
 		return null;
 	}
--- src/ie/atm/Screen.java
+++ src/ie/atm/Screen.java
@@ -1,5 +1,9 @@
 package ie.atm;
 
 public class Screen {
+	
+	public void displayMessageLine(String message){
+		System.out.println(message);
+	}
 
 }
